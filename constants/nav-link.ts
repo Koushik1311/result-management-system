2fc49a4b6ba6b96e1678d5fcb54aafbdf46edfa0
Koushik <programmer.koushik@gmail.com>
2024-09-12 23:30:22 +0530

    nav links

---
+++ constants/nav-link.ts
@@ -0,0 +1,14 @@
+export const NavLinksData = [
+  {
+    name: "Home",
+    uri: "/",
+  },
+  {
+    name: "About us",
+    uri: "/about",
+  },
+  {
+    name: "contact us",
+    uri: "/contact",
+  },
+];
