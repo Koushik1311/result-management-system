5630e5bcff75962c2c8be27532e320e350b8b95f
Koushik <programmer.koushik@gmail.com>
2024-09-21 13:08:36 +0530

    dashboard links

---
+++ constants/dashboard-link.ts
@@ -0,0 +1,24 @@
+// import { HiOutlineDocumentReport } from "react-icons/hi";
+
+export const DashboardLinksData = [
+  {
+    name: "Attendance",
+    uri: "/dashboard/attendance",
+  },
+  {
+    name: "Project Review",
+    uri: "/dashboard/project-review",
+  },
+  {
+    name: "Assessment",
+    uri: "/dashboard/assessment",
+  },
+  {
+    name: "Project Submission",
+    uri: "/dashboard/project-submission",
+  },
+  {
+    name: "LinkedIn Post",
+    uri: "/dashboard/linkedin-post",
+  },
+];
