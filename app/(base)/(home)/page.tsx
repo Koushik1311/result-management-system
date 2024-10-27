e5d34d659ed28997cba448e0f9d954ea477128db
Koushik <programmer.koushik@gmail.com>
2024-10-27 19:05:28 +0530

    home page

--- app/(base)/(home)/page.tsx
+++ app/(base)/(home)/page.tsx
@@ -1,3 +1,36 @@
+import Link from "next/link";
+
 export default function HomePage() {
-  return <div>page</div>;
+  return (
+    <div className="container flex flex-col items-center justify-center min-h-screen p-6 bg-gray-100">
+      <h1 className="text-center text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-blue-600 to-violet-400">
+        <span className="block text-5xl bg-clip-text text-transparent bg-gradient-to-r from-violet-400 to-blue-500">
+          Your
+        </span>
+        <span className="block text-5xl bg-clip-text text-transparent bg-gradient-to-r from-violet-400 to-blue-500">
+          <span className="block">Result</span>
+          <span className="block">Management Platform</span>
+        </span>
+      </h1>
+      <p className="mt-4 text-lg text-gray-700">
+        Simplifying result management with efficient and user-friendly
+        solutions.
+      </p>
+
+      <div className="mt-6 flex gap-4">
+        <Link
+          href="/result"
+          className="px-6 py-2 text-white bg-blue-500 rounded-full shadow hover:bg-blue-600 transition duration-300"
+        >
+          Check Results
+        </Link>
+        <Link
+          href="/login"
+          className="px-6 py-2 text-white bg-violet-500 rounded-full shadow hover:bg-violet-600 transition duration-300"
+        >
+          Manage Results
+        </Link>
+      </div>
+    </div>
+  );
 }
