8cad2527c0635e1170706247c9b19df82058debe
Koushik <programmer.koushik@gmail.com>
2024-10-27 19:05:21 +0530

    dashboard layout

--- components/layout/Dashboard.tsx
+++ components/layout/Dashboard.tsx
@@ -1,17 +1,34 @@
 import Logo from "../global/Logo";
 import DashboardTab from "../dashboard/DashboardTab";
+import { getUserData, logoutUser } from "@/data/auth/authentication";
+import { redirect } from "next/navigation";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const userData = await getUserData();
+
+  const logoutButtonClick = async () => {
+    "use server";
+
+    logoutUser();
+
+    redirect("/login");
+  };
+
   return (
     <div className="container">
       {/* Dashboard */}
       <div className="flex items-center justify-between py-4">
         <Logo />
         <div className="flex items-center gap-4">
-          <p>Koushik Roy</p>
-          <button className="text-violet-600 hover:text-violet-500">
-            Log Out
-          </button>
+          <p>{userData.data.fullName}</p>
+          <form>
+            <button
+              formAction={logoutButtonClick}
+              className="text-violet-600 hover:text-violet-500"
+            >
+              Log Out
+            </button>
+          </form>
         </div>
       </div>
       <DashboardTab />
